d1c714e2f7f9d7623ba48055541af6f283797f7f
chin2555 <120727299+chin2555@users.noreply.github.com>
2023-09-25 21:59:02 +0800

    Update model.py

--- app/model.py
+++ app/model.py
@@ -3,10 +3,11 @@ import pickle
 import gzip
 import numpy as np
 
-with gzip.open('app/model/random_forest_model.pgz', 'r') as f:
+# 載入Model
+with gzip.open('app/model/random_forest_model_north.pgz', 'r') as f:
     rfModel = pickle.load(f)
 
-with gzip.open('app/model/min_max_scaler.pgz', 'r') as f:
+with gzip.open('app/model/min_max_scaler_north.pgz', 'r') as f:
     scaler = pickle.load(f)
 
 def predict(input):
