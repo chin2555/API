eaf7815790179760a74750bd26965a684211adfd
chin2555 <120727299+chin2555@users.noreply.github.com>
2023-10-02 22:05:55 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ def getResult():
     result = model.predict(input)
     return jsonify({'result':str(result)})
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predictNorth', methods=['POST'])
 def postInput():
     # 取得前端傳的值
     forecastData = request.get_json()
@@ -30,7 +30,30 @@ def postInput():
         x7 = forecast['月份']
         
         input_data = np.array([[x1, x2, x3, x4, x5, x6, x7]])
-        result = model.predict(input_data)
+        result = model.predictNorth(input_data)
+        results.append({'result': str(result)})
+
+    return jsonify(results)
+
+
+@app.route('/predictMiddle', methods=['POST'])
+def postInput():
+    # 取得前端傳的值
+    forecastData = request.get_json()
+
+    # 遍歷預報數據
+    results = []
+    for forecast in forecastData:
+        x1 = forecast['氣壓']
+        x2 = forecast['最高溫度']
+        x3 = forecast['最低溫度']
+        x4 = forecast['相對濕度']
+        x5 = forecast['風速']
+        x6 = forecast['雲量']
+        x7 = forecast['月份']
+        
+        input_data = np.array([[x1, x2, x3, x4, x5, x6, x7]])
+        result = model.predictMiddle(input_data)
         results.append({'result': str(result)})
 
     return jsonify(results)
